0d919e19997dca2d9123bdf004d64124d8f5fbbc
adamtbengtsson <61778432+adamtbengtsson@users.noreply.github.com>
2020-03-11 16:36:21 +0100

    Update models.py

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ def load_user(user_id):
 	return User.query.get(int(user_id))
 
 class DB_handler(): #hanterar det som ska skickas till och från databasen, om vi skulle ha det i t.ex car_manager skulle den bli för stor.
-	__innit__(self):
+	__init__(self): # Tror inte vi ska ha någon sådan klass
 	
 
 @dataclass
@@ -82,47 +82,3 @@ class Token(db.Model):
 	date_expired = db.Column(db.DateTime, nullale=False)
 	token = db.Column(db.String(60), nullable=False, index=True)
 	
-
-class TimeInterval:
-
-    def __init__(self, start_date, end_date):
-        self.end_date = end_date
-        self.start_date = start_date
-
-    def is_not_in_interval(self, requested_start_date: datetime, requested_end_date: datetime) -> bool:
-        return (requested_start_date < self.start_date and requested_end_date < self.start_date) \
-               or (requested_start_date > self.start_date and requested_end_date > self.end_date)
-	
-	
-class RentableCar(Car):  # ska inherita från car
-
-    def __init__(self):
-        self.booked_dates = []
-
-
-    def add_booked_interval(self, interval):
-        self.booked_dates.append(interval)
-
-    def is_available(self, requested_interval: TimeInterval) -> bool:
-
-        if len(self.booked_dates) == 0:
-            return True
-
-        for interval in self.booked_dates:
-            if interval.is_not_in_interval(requested_interval.start_date, requested_interval.end_date):
-                return True
-
-        return False
-
-
-first_date = input("Enter first date(YYYY-MM-DD): ")
-second_date = input("Enter last date(YYYY-MM-DD): ")
-test_interval = TimeInterval(datetime.fromisoformat(first_date), datetime.fromisoformat(second_date))
-
-
-car = RentableCar()
-if car.is_available(test_interval):
-    print(f"Successfully booked {first_date} to {second_date}!")
-    car.add_booked_interval(test_interval)
-else:
-    print("The dates you have selected are not available, please try again.")
